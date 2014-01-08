f8a088d10acb9b34cb453e465e14277101a05ee1
Daniel Nordstrom <d@nintera.com>
2014-01-08 02:45:23 +0100

    Static linkage.

---
+++ polestar/plugins/polestar.staticlinks.js
@@ -0,0 +1,59 @@
+/**
+ * Adds links to static versions of Polestar articles, assuming they
+ * are located in a `/static` subdirectory. Later, it should
+ * obviously be possible to specify this directory.
+ *
+ * @author  L. Daniel Nordstrom <d@mrnordstrom.com>
+ * @version 0.0.1
+ * @license MPL 2.0
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * This function is called for each article, in the context of the
+ * object representing the article. This object has properties for
+ * the ID (`id`), content (`content`), and HTML node (`element`).
+ *
+ * @method
+ */
+Polestar.Staticlinks = {
+  afterRender: function () {
+    var permalink = document.createElement('a')
+    var dateMatches =
+        /^(\d{4})-?(\d{2})-?(\d{2}).+/.exec(this.id)
+      
+    if (dateMatches) {
+      var monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ]
+
+      var date = new Date(dateMatches.splice(1).join('-'))
+      var dateString =
+        monthNames[date.getMonth()] + ' ' +
+        date.getDate() + ', ' +
+        date.getFullYear()
+
+      permalink.appendChild(document.createTextNode(dateString))
+    } else {
+      permalink.appendChild(document.createTextNode('#'))
+    }
+
+    permalink.setAttribute('class', 'permalink')
+    permalink.setAttribute('href', 'static/' + this.id)
+    this.element.appendChild(permalink)
+  }
+}
